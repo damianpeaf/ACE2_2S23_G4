9f8f14efee853ecf4bb122bbd078d28bf9460ab2
Damian <damianpeaf@gmail.com>
2023-08-02 20:25:43 -0600

    feat(Practica1): :safety_vest: sensor measurements

--- Practica 1/weather_station/weather_station.ino
+++ Practica 1/weather_station/weather_station.ino
@@ -1,11 +1,42 @@
+#include <DHT.h>
+#include <DHT_U.h>
+
+
+// ----------------- DHT11 -----------------
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// ----------------- LDR -----------------
+#define LDRPIN A0
+
+// ----------------- MQ135 -----------------
+#define MQ135PIN A1
+
+
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+
+  dht.begin();
+
+  pinMode(LDRPIN, INPUT);
+  pinMode(MQ135PIN, INPUT);
 
 }
 
 void loop() {
   // Will be streamed temperature;humidity;air quality;light\n
-  Serial.println("25;50;100;1000");
+
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  int light = analogRead(LDRPIN);
+
+  int airQuality = analogRead(MQ135PIN);
+
+  Serial.println(String(temperature)+";"+String(humidity)+";"+String(airQuality)+";"+String(light));
+
   delay(1000);
 }
