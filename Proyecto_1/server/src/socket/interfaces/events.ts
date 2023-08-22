c6ad054dc980fe90440b3bd943399261be083cff
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2023-08-21 19:01:23 -0600

    feat(Proyecto1): :fire: events interfaces for sockets

---
+++ Proyecto_1/server/src/socket/interfaces/events.ts
@@ -0,0 +1,57 @@
+export type VentState = 'off' | 'vel_1' | 'vel_2';
+
+export enum AppEventType {
+    Sync = 'global_state_sync',
+    LiveData = 'live_data',
+    Notification = 'notification',
+    // output events
+    LightChange = 'light_change',
+    VentChange = 'vent_change',
+}
+
+export type AppEvent = {
+    type: AppEventType;
+    payload: any;
+};
+
+export type SyncEvent = {
+    type: AppEventType.Sync;
+    payload: {
+        is_light_on: boolean;
+        vent_state: VentState;
+    };
+};
+
+export type LiveDataEvent = {
+    type: AppEventType.LiveData;
+    payload: {
+        temperature: number;
+        light: number;
+        air_quality: number;
+        timestamp: string;
+        presence: boolean;
+    };
+};
+
+export type NotificationEvent = {
+    type: AppEventType.Notification;
+    payload: {
+        type: 'warning' | 'error' | 'info';
+        message: string;
+        timestamp: string;
+    };
+};
+
+export type LightChangeEvent = {
+    type: AppEventType.LightChange;
+    payload: {
+        is_light_on: boolean;
+    };
+};
+
+export type VentChangeEvent = {
+    type: AppEventType.VentChange;
+    payload: {
+        vent_state: VentState;
+    };
+};
