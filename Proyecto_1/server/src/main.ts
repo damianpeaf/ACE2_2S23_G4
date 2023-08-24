f98673023f4805b6e60b82e760553ac8e2614fc4
Damian <damianpeaf@gmail.com>
2023-08-23 19:39:46 -0600

    fix(Proyecto1): :passport_control: deploy port

--- Proyecto_1/server/src/main.ts
+++ Proyecto_1/server/src/main.ts
@@ -4,7 +4,7 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, '0.0.0.0');
-  Logger.log('Server running on 0.0.0.0', 'Bootstrap');
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  Logger.log('Server running on 0.0.0.0 and port: ' + process.env.PORT || 3000, 'Bootstrap');
 }
 bootstrap();
