7a87b622ed85d0ea03217ef0f2b8f68038e717fd
3009610220101 <3009610220101@ingenieria.usac.edu.gt>
2023-09-19 12:13:26 -0600

    Update astro.config.mjs

--- Practica_2/astro.config.mjs
+++ Practica_2/astro.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
 						{ label: 'Descripcion', link: '/descripcion/' },
 						{ label: 'Bocetos del Prototipo', link: '/bocetos/' },
 						{ label: 'Prototipo', link: '/imagenes/' },
-						{ label: 'Mock-Ups', link: '/mockups/' },
+						{ label: 'Mock-Ups', link: '/mockups/' }
 					],
 				},
 			],
