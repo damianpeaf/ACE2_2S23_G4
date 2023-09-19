ea5c6ed43edceff83e6f2247900ebe992bcde2f0
3009610220101 <3009610220101@ingenieria.usac.edu.gt>
2023-09-19 11:29:30 -0600

    Prueba 2.0

--- Practica_2/astro.config.mjs
+++ Practica_2/astro.config.mjs
@@ -11,11 +11,32 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
-					label: 'Example',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/example/' },
-					],
+					label: "Descripcion",
+					autogenerate: { directory: "descripcion" },
+				},
+				{
+					label: "Bocetos",
+					autogenerate: { directory: "bocetos" },
+				},
+				{
+					label: "Imagenes del Prototipo",
+					autogenerate: { directory: "imagenesPrototipo" },
+				},
+				{
+					label: "MockUps de Aplicaciones",
+					autogenerate: { directory: "mockUps" },
+				},
+				{
+					label: "Smart Connected Design Framework",
+					autogenerate: { directory: "smartConnected" },
+				},
+				{
+					label: "Diagramas",
+					autogenerate: { directory: "diagramas" },
+				},
+				{
+					label: "MQTT",
+					autogenerate: { directory: "mqtt" },
 				},
 			],
 		}),
